958e6a11658233f771cc1abaffedab7b88d19249
abrarkhanmokashi <abrarkhanmokashi@eng.rizvi.edu.in>
2026-02-22 13:09:42 +0530

    Implement word extraction by specified length
    Added functionality to read a text file and extract words of a specified length.

--- main.py
+++ main.py
@@ -5,4 +5,13 @@
 # Date:
 
 print("--- Extracting Words from Text File ---\n")
+with open("story.txt","r") as file:
+  text=file.read().split()
+  length=int(input("Enter Length of Words: "))
+  list1=[]
+  for x in text:
+    if (len(x)==length):
+      list1.append(x.lower())
+  new_list=sorted(set(list1))
+  print(f"Words with length {length} are: {new_list}")
 
